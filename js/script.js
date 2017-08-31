0aaaf672889e4f35f0091ec249111099f4a5d387
Jeff Ripke <jripke74@gmail.com>
2017-08-31 06:17:24 -0700

    add array

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-var html = '';
-var rgbColor;
-
-function randomRGB() {
-  return Math.floor(Math.random() * 256);
-}
-
-function randomColor() {
-  var color = 'rgb(';
-  color += randomRGB() + ',';
-  color += randomRGB() + ',';
-  color += randomRGB() + ')';
-  return color;
-}
-
-function print(message) {
-  document.write(message);
-}
-
-for (var i = 0; i < 100; i += 1) {
-  rgbColor = randomColor();
-  html += '<div style="background-color:' + rgbColor + '"></div>';
-}
-
-print(html);
+var shopping = [
+  'carrots',
+  'milk',
+  'eggs'
+];
+console.log(shopping[0]);
+console.log(shopping[1]);
+console.log(shopping[2]);
+// this will respond with undefined because it does not exist in the array
+console.log(shopping[3]);
