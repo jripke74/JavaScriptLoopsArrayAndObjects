52753f61c75b6ba4e21e0139fc6919e33357e643
Jeff Ripke <jripke74@gmail.com>
2017-09-01 08:31:51 -0700

    add object challenge

--- js/script.js
+++ js/script.js
@@ -1,48 +1,7 @@
-var questionList = [
-  { question: 'How many legs do dogs have?', answer: 4 },
-  { question: 'How old am I?', answer: 43 },
-  { question: 'How many kids do I have?', answer: 3 },
-  { question: 'Is the moon bigger than the earth?', answer: 0 }
+var students = [
+  { name: 'Jeff', track: 'iOS', achievements: 40, points: 22000 },
+  { name: 'Jason', track: 'Front End Development', achievements: 3, points: 2333 },
+  { name: 'Ryan', track: 'iOS', achievements: 2, points: 1900 },
+  { name: 'Matthew', track: 'iOS', achievements: 3, points: 2433 },
+  { name: 'David', track: 'iOS', achievements: 3, points: 2544 }
 ];
-var numberOfCorrectAnswers = 0;
-var question;
-var answer;
-var response;
-var html;
-var correct = [];
-var wrong = [];
-
-// print(message) function is used to send HTML to the browser
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-}
-
-function buildList(arr) {
-  var listHTML = '<ol>';
-  for (var i = 0; i < arr.length; i++) {
-    listHTML += '<li>' + arr[i] + '</li>';
-  }
-  listHTML += '</ol>';
-  return listHTML;
-}
-
-for (var i = 0; i < questionList.length; i++) {
-  question = questionList[i].question;
-  answer = questionList[i].answer;
-  response = prompt(question);
-  response = parseInt(response);
-  if (response === answer) {
-    numberOfCorrectAnswers += 1;
-    correct.push(question);
-  } else {
-    wrong.push(question);
-  }
-
-}
-html = "<h1>You got " + numberOfCorrectAnswers + " question(s) correct.</h1>"
-html += '<h2>You got thise questions correct:</h2>';
-html += buildList(correct);
-html += '<h2>You got these question wrong:</h2>';
-html += buildList(wrong);
-print(html);
